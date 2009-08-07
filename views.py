1042861bcc5b99a47b75c0430b23b38083fae5e8
biouser <skylar.saveland@gmail.com>
2009-08-07 10:57:29 -0400

    Add view present, gitignore managed

--- views.py
+++ views.py
@@ -2,9 +2,10 @@ from django.views.generic.list_detail import object_list, object_detail
 from django.views.generic.create_update import create_object, delete_object
 from django.contrib.auth.decorators import login_required
 from django.core.urlresolvers import reverse
-from django.shortcuts import get_object_or_404
+from django.shortcuts import get_object_or_404, render_to_response
 from django.contrib.contenttypes.models import ContentType
-from django.http import HttpResponseNotFound
+from django.http import HttpResponseNotFound, HttpResponseRedirect
+from django.template import RequestContext
 
 from events.models import Event
 from events.forms import EventForm
@@ -58,4 +59,27 @@ def add(request, app_label, model_name, id):
 
     except:
         return HttpResponseNotFound()
+
+    if request.method == 'POST':
+        request.POST.update( { 'owner':request.user.id, 'object_id':id,
+                'content_type':ct.id, 'content_obj': obj, } )
+        form = EventForm(request.POST)
+
+        if form.is_valid():
+            form.save()
+
+        try:
+            return HttpResponseRedirect(obj.get_absolute_url())
+
+        except:
+            return HttpResponseRedirect(reverse('events_all'))
+
+    else:
+        form = EventForm()
+
+    context = { 'form':form, 'object':obj, 'content_type':ct, }
+    context.update(locals())
+
+    return render_to_response('events/event_form.html', context,\
+            context_instance = RequestContext(request))
 
