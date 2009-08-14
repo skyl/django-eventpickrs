c8bd0eda54bf2a955ede2720a317b7c56ab1d635
Skylar Saveland <skylar.saveland@gmail.com>
2009-08-13 22:39:40 -0400

    made search work across all list views

--- views.py
+++ views.py
@@ -167,7 +167,10 @@ def for_instance(request, app_label, model_name, id):
 
     events = Event.objects.filter(content_type = ct, object_id = id)
 
+    search_form = SearchForm()
+
     return object_list(request,
-            queryset = events
+            queryset = events,
+            extra_context = locals(),
     )
 
